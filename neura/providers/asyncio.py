6b1ea2e0ad1c01dcd4ebd161beb5de6c915ccd7d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-29 11:49:42 +0530

    neura: providers[asyncio]

---
+++ neura/providers/asyncio.py
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------
+# Copyright [2025] [Krisna Pranav, Neura AI]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# -----------------------------------------------------------------------------
+
+import asyncio
+from asyncio import AbstractEventLoop, runners
+from typing import Optional, Callable, AsyncIterator, Iterator
+from __future__ import annotations
+from ..errors import NestAsyncioError
+
+
+try:
+    import nest_asyncio
+    has_nest_asyncio = True
+except ImportError:
+    has_nest_asyncio = False
+
+try:
+    import uvloop
+    has_uvloop = True
+except ImportError:
+    has_uvloop = False
+    
