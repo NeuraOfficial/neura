927164c09ca80df5f3ed7eda73bf87f82882d283
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-02-04 13:22:26 +0530

    neura: providers[retry_provider]

--- neura/providers/retry_provider.py
+++ neura/providers/retry_provider.py
@@ -24,38 +24,155 @@ from ..errors import RetryProviderError, RetryNoProviderError
 
 class IterListProvider(BaseRetryProvider):
     def __init__(self, providers: List[Type[BaseProvider]], shuffle: bool = True) -> None:
-        self.provider = providers
+        self.providers = providers
         self.shuffle = shuffle
         self.working = True
-        self.last_provider = Type[BaseProvider] = None
-    
-    def create_completion(self, model: str, messages: Messages, stream: bool = False, ignore_stream: bool = False, ignored: list[str] = [], **kwargs):
+        self.last_provider: Type[BaseProvider] = None
+
+    def create_completion(self, model: str, messages: Messages, stream: bool = False, ignore_stream: bool = False, ignored: list[str] = [], **kwargs,) -> CreateResult:
         exceptions = {}
         started: bool = False
-        
-        for provider in self.get_providers(stream and not ignore_stream):
+
+        for provider in self.get_providers(stream and not ignore_stream, ignored):
             self.last_provider = provider
             debug.log(f"Using {provider.__name__} provider")
-            yield ProviderInfo(**provider.get_dict(), model=model)
+            
+            yield ProviderInfo(**provider.get_dict(), model=model if model else getattr(provider, "default_model"))
+            
             try:
-                response = provider.get_create_function()(model)
-                for chunk in response :
+                response = provider.get_create_function()(model, messages, stream=stream, **kwargs)
+                for chunk in response:
                     if chunk:
                         yield chunk
-                        
-                        if isinstance(chunk, str):
+                        if isinstance(chunk, str) or isinstance(chunk, ImageResponse):
                             started = True
-                    
+                if started:
+                    return
+            except Exception as e:
+                exceptions[provider.__name__] = e
+                debug.log(f"{provider.__name__}: {e.__class__.__name__}: {e}")
+                if started:
+                    raise e
+                yield e
+
+        raise_exceptions(exceptions)
+
+    async def create_async_generator(self, model: str, messages: Messages, stream: bool = True, ignore_stream: bool = False, ignored: list[str] = [], **kwargs) -> AsyncResult:
+        exceptions = {}
+        started: bool = False
+
+        for provider in self.get_providers(stream and not ignore_stream, ignored):
+            self.last_provider = provider
+            debug.log(f"Using {provider.__name__} provider")
+            
+            yield ProviderInfo(**provider.get_dict())
+            
+            try:
+                response = provider.get_async_create_function()(model, messages, stream=stream, **kwargs)
+                if hasattr(response, "__aiter__"):
+                    async for chunk in response:
+                        if chunk:
+                            yield chunk
+                            if isinstance(chunk, str) or isinstance(chunk, ImageResponse):
+                                started = True
+                elif response:
+                    response = await response
+                    if response:
+                        yield response
+                        started = True
+                if started:
+                    return
+            except Exception as e:
+                exceptions[provider.__name__] = e
+                debug.log(f"{provider.__name__}: {e.__class__.__name__}: {e}")
+                if started:
+                    raise e
+                yield e
+
+        raise_exceptions(exceptions)
+
+    def get_create_function(self) -> callable:
+        return self.create_completion
+
+    def get_async_create_function(self) -> callable:
+        return self.create_async_generator
+
+    def get_providers(self, stream: bool, ignored: list[str]) -> list[ProviderType]:
+        providers = [p for p in self.providers if (p.supports_stream or not stream) and p.__name__ not in ignored]
+        if self.shuffle:
+            random.shuffle(providers)
+        return providers
+
+class RetryProvider(IterListProvider):
+    def __init__(self, providers: List[Type[BaseProvider]], shuffle: bool = True, single_provider_retry: bool = False,max_retries: int = 3,) -> None:
+        super().__init__(providers, shuffle)
+        self.single_provider_retry = single_provider_retry
+        self.max_retries = max_retries
+
+    def create_completion(self, model: str, messages: Messages, stream: bool = False, **kwargs,) -> CreateResult:
+        if self.single_provider_retry:
+            exceptions = {}
+            started: bool = False
+            provider = self.providers[0]
+            self.last_provider = provider
+            
+            for attempt in range(self.max_retries):
+                try:
+                    if debug.logging:
+                        print(f"Using {provider.__name__} provider (attempt {attempt + 1})")
+                    response = provider.get_create_function()(model, messages, stream=stream, **kwargs)
+                    for chunk in response:
+                        if isinstance(chunk, str) or isinstance(chunk, ImageResponse):
+                            yield chunk
+                            started = True
                     if started:
                         return
-            except Exception as e:
-                yield e
-            
-            
+                except Exception as e:
+                    exceptions[provider.__name__] = e
+                    if debug.logging:
+                        print(f"{provider.__name__}: {e.__class__.__name__}: {e}")
+                    if started:
+                        raise e
+            raise_exceptions(exceptions)
+        else:
+            yield from super().create_completion(model, messages, stream, **kwargs)
+
+    async def create_async_generator(self, model: str, messages: Messages, stream: bool = True, **kwargs) -> AsyncResult:
+        exceptions = {}
+        started = False
+
+        if self.single_provider_retry:
+            provider = self.providers[0]
+            self.last_provider = provider
+            for attempt in range(self.max_retries):
+                try:
+                    debug.log(f"Using {provider.__name__} provider (attempt {attempt + 1})")
+                    response = provider.get_async_create_function()(model, messages, stream=stream, **kwargs)
+                    if hasattr(response, "__aiter__"):
+                        async for chunk in response:
+                            if isinstance(chunk, str) or isinstance(chunk, ImageResponse):
+                                yield chunk
+                                started = True
+                    else:
+                        response = await response
+                        if response:
+                            yield response
+                            started = True
+                    if started:
+                        return
+                except Exception as e:
+                    exceptions[provider.__name__] = e
+                    if debug.logging:
+                        print(f"{provider.__name__}: {e.__class__.__name__}: {e}")
+            raise_exceptions(exceptions)
+        else:
+            async for chunk in super().create_async_generator(model, messages, stream, **kwargs):
+                yield chunk
+                
 def raise_exceptions(exceptions: dict) -> None:
     if exceptions:
-        raise RetryProviderError("RetryProvider failed: \n", + "\n".join([
-            f"{p}: {type(exception).__name__} : {exception}"
+        raise RetryProviderError("RetryProvider failed:\n" + "\n".join([
+            f"{p}: {type(exception).__name__}: {exception}" for p, exception in exceptions.items()
         ]))
-        
+
     raise RetryNoProviderError("No provider found")
